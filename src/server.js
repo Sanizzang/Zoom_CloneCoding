5255acd6f59b993b4ceb3719a89124de0e03c4f2
Sanizzang <tksl4732@naver.com>
2023-03-09 13:32:20 +0900

    webSocket 구현

---
+++ src/server.js
@@ -0,0 +1,36 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/*", (req, res) => res.redirect("/"));
+
+app.get("/", (req, res) => res.render("home")); 
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon";
+
+    console.log("Connected to Browser");
+    socket.on("close", () => console.log("DisConnected from Server")); 
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+    });
+});
+
+server.listen(3000);
